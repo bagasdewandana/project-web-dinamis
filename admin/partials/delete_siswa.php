d2354b8515477f97dad28cb0f7e11fcdb4928bbd
premaagus <premaagus@gmail.com>
2018-04-30 11:36:05 +0800

    Delete Siswa Bug

--- admin/partials/delete_siswa.php
+++ admin/partials/delete_siswa.php
@@ -2,6 +2,9 @@
 	$id_siswa = $_GET['id_siswa'];
 	$query_delete = $koneksi->query("DELETE FROM tb_siswa WHERE id_siswa = $id_siswa");
 	if ($query_delete) {
-		header('location: ?menu=data_siswa');
+		$query_delete = $koneksi->query("DELETE FROM tb_uangkas WHERE id_siswa = $id_siswa");
+		if ($query_delete) {
+			header('location: ?menu=data_siswa');
+		}
 	}
  ?>
