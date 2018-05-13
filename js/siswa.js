6ef637a50e6b034b15dc56c4319f981f0b36fe91
devamahayana <devamahayanatop@gmail.com>
2018-05-13 21:38:11 +0800

    baru siswa

--- js/siswa.js
+++ js/siswa.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-$(document).ready(function(){
-    $("#viewMore").click(function(){
-        $("#info").animate({top:"460px"}) ;
-        $("#aboutMe").animate({top:"0px"});
-        $("#image").animate({left:"250px"});
-        $("#project").animate({left:"0px"});
-    }); 
-    
-    $("#close").click(function(){
-        $("#info").animate({top:"280px"}) ;
-        $("#aboutMe").animate({top:"-160px"});
-        $("#image").animate({left:"0px"});
-        $("#project").animate({left:"-250px"});
-    }); 
-});
->>>>>>> parent of 2346a80... adding js siswa
 
 
 
@@ -26,7 +6,3 @@ $(document).ready(function(){
 
 
 
-<<<<<<< HEAD
->>>>>>> 74841eac25cb7f4634489f66df15c5156ef74de4
-=======
->>>>>>> parent of 2346a80... adding js siswa
--- siswa.php
+++ siswa.php
@@ -36,18 +36,10 @@
 					                <div id="wave2"></div>
 					            </div>
 					            
-<<<<<<< HEAD
-					            <div id="info" class="info<?= $no ?>">
-					                <div id="viewMore" class="viewMore<?= $no ?>"><img src="img/siswa/View%20More.png" width="70%"/></div>
 					            <div id="info">
 					                <div id="viewMore">
 					                	<a href="
 					                	admin/index.php"><img  src="img/siswa/View%20More.png" width="70%"/></a></div>
-
-=======
-					            <div id="info">
-					                <div id="viewMore"><img src="img/siswa/View%20More.png" width="70%"/></div>
->>>>>>> parent of 2346a80... adding js siswa
 					                <div id="name"><?= $data_siswa['nama_siswa'] ?></div>
 					                <div id="status"><?= '" ' . $data_siswa['quotes'] . ' "'?></div>
 					                <div id="social">
@@ -57,51 +49,9 @@
 					                </div>
 					            </div>
 					            
-<<<<<<< HEAD
-
-					            <div id="aboutMe" class="aboutMe<?= $no ?>">
-=======
-					            <div id="aboutMe">
->>>>>>> parent of 2346a80... adding js siswa
-					                <h2>ABOUT ME</h2>
-					                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Loren Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-					                <div id="close"><img src="img/siswa/Close.png" width="65%"/></div>
-					            </div>
 					            
-					            <div id="project">
-					                <h2>PROJECT</h2>
-					                <div id="panel1">
-					                    <div class="projectImg"><img src="img/siswa/Project1.jpg"/></div>
-					                    <div class="projectTitle">DAFT WEB DESIGN</div>
-					                    <div class="projectTools">
-					                        <div class="visited"><img src="img/siswa/Watch.png"/>120</div>
-					                        <div class="like"><img src="img/siswa/Like.png"/>80</div>
-					                        <div class="visit"><img src="img/siswa/Visit.png"/></div>
-					                    </div>
-					                    <hr size="1" width="80%" color="#ccc"/>
-					                </div>
-					                
-					                <div id="panel2">
-					                    <div class="projectImg"><img src="img/siswa/Project2.jpg"/></div>
-					                    <div class="projectTitle">MOVIE MAGIC</div>
-					                    <div class="projectTools">
-					                        <div class="visited"><img src="img/siswa/Watch.png"/>90</div>
-					                        <div class="like"><img src="img/siswa/Like.png"/>67</div>
-					                        <div class="visit"><img src="img/siswa/Visit.png"/></div>
-					                    </div>
-					                    <hr size="1" width="80%" color="#ccc"/>
-					                </div>
-					                
-					                <div id="panel3">
-					                    <div class="projectImg"><img src="img/siswa/Project3.jpg"/></div>
-					                    <div class="projectTitle">MULTITECH</div>
-					                    <div class="projectTools">
-					                        <div class="visited"><img src="img/siswa/Watch.png"/>75</div>
-					                        <div class="like"><img src="img/siswa/Like.png"/>56</div>
-					                        <div class="visit"><img src="img/siswa/Visit.png"/></div>
-					                    </div>
-					                </div>
-					            </div>
+					            
+					            
 					        </div>
 				 		</div><!--col-lg-3 main-holder-->
  						<?php
@@ -116,36 +66,4 @@
  	</div><!--grid-->
  </div><!--wrapper-->
  </body>
-<<<<<<< HEAD
- <script type="text/javascript">
-  $(document).ready(function(){
-    $bg_menu_li = $('.menu-kiri li a, .menu-kanan li a');
-    $menu_line = $('#MenuLine, #MenuLine1, #MenuLine2');
-    $menu_line.css({'background-color': 'black'});
-    $bg_menu_li.css({'color':'black'});
-  });
-  $(window).scroll(function(){
-    $bg_menu = $('.bg-menu');
-    $wScroll = $(window).scrollTop();
-    $menu_height = $bg_menu.outerHeight();
-    $bg_menu_li = $('.menu-kiri li a, .menu-kanan li a');
-    $top_logo = $('.top-logo');
-    $garis = $('.garis');
-    $menu_line = $('#MenuLine, #MenuLine1, #MenuLine2');
-    if ($wScroll>$menu_height) {
-      $bg_menu_li.css({'color':'white'});
-      $garis.css({'background': 'white'});
-      $menu_line.css({'background-color': 'white'});
-      $bg_menu.css({'background':'#E2B512'});
-      $top_logo.css({'background-image':"url(img/3.png)"});
-    }
-    else{
-      $bg_menu_li.css({'color':'#000'});
-      $garis.css({'background': '#000'});
-      $menu_line.css({'background-color': 'black'});
-      $bg_menu.css({'background':'transparent'});
-      $top_logo.css({'background-image':"url(img/31.png)"});
-    }
-  });
-</script>
  </html>
