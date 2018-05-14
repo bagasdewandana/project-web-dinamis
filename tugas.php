821c486d20806bd24fa972523ab67c2b7371e353
Prema Agus <premaagus@gmail.com>
2018-05-13 22:24:12 -0700

    buat file tugas.php

---
+++ tugas.php
@@ -0,0 +1,35 @@
+<?php 
+	require_once 'koneksi/koneksi.php';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+	<title>Repalti</title>
+	<link rel="icon" href="img/31.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/tugas.css?v=0.1" type="text/css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fontawesome.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fontawesome.min.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-solid.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-solid.min.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-regular.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-regular.min.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-brands.css">
+	<link rel="stylesheet" href="fontawesome/font-css/css/fa-brands.min.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<script src="js/jquery-3.2.1.min.js"> </script>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/JQUERY%20Main.js"></script>
+ </head>
+ <body>
+ 	<div class="wrapper">
+ 		<?php 
+ 			require_once 'partials/navbar.inc.php';
+ 		 ?>
+ 	</div><!-- wrapper -->
+ </body>
+ </html>
